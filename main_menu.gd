9a88d1e9a49bb63c6dfbb0fe93e7494071584898
Jennie W <125776035+ProbAPerson@users.noreply.github.com>
2026-02-18 20:23:36 -0500

    Add files via upload

---
+++ main_menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_1.tscn")
